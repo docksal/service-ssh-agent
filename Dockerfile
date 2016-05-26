3e157308bbc420097c096c8742bde18f3177acdc
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-05-26 12:52:01 -0400

    proxy-socket via socat with open access for ssh-agent

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM alpine:3.2
 
 ENV SOCKET_DIR /.ssh-agent
 ENV SSH_AUTH_SOCK ${SOCKET_DIR}/socket
-RUN apk add --update openssh && rm -rf /var/cache/apk/*
+ENV SSH_AUTH_PROXY_SOCK ${SOCKET_DIR}/proxy-socket
+RUN apk add --update openssh socat && rm -rf /var/cache/apk/*
 COPY run.sh /run.sh
 VOLUME ${SOCKET_DIR}
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 case "$1" in
-    ssh-agent)
-        exec /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -d > /dev/null 2>&1
-        ;;
-    *)
-        exec $@;;
+	ssh-agent)
+		# Create proxy-socket for ssh-agent (to give anyone accees to the ssh-agent socket)
+		echo "Creating proxy socket..."
+		rm ${SSH_AUTH_PROXY_SOCK} > /dev/null 2>&1
+		socat UNIX-LISTEN:${SSH_AUTH_PROXY_SOCK},perm=0666,fork UNIX-CONNECT:${SSH_AUTH_SOCK} &
+		echo "Launching ssh-agent..."
+		exec /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -d
+		;;
+	*)
+		exec $@;;
 esac
