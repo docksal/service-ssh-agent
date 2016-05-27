8fdf1c6be8d91e463508d98a68902c6c1c01c260
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-05-26 20:19:06 -0400

    Remove both socket files on start

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ case "$1" in
 	ssh-agent)
 		# Create proxy-socket for ssh-agent (to give anyone accees to the ssh-agent socket)
 		echo "Creating proxy socket..."
-		rm ${SSH_AUTH_PROXY_SOCK} > /dev/null 2>&1
+		rm ${SSH_AUTH_SOCK} ${SSH_AUTH_PROXY_SOCK} > /dev/null 2>&1
 		socat UNIX-LISTEN:${SSH_AUTH_PROXY_SOCK},perm=0666,fork UNIX-CONNECT:${SSH_AUTH_SOCK} &
 		echo "Launching ssh-agent..."
 		exec /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -d
