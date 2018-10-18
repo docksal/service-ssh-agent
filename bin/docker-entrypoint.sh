d8a6a76cf7794dd196bc58870bdce0599651e2d4
Leonid Makarov <leonid.makarov@gmail.com>
2018-10-17 21:25:58 -0700

    Don't fail on "rm" in docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -9,7 +9,7 @@ mkdir -p ${SSH_DIR}
 if [[ "$1" == "ssh-agent" ]]; then
 	# Create proxy-socket for ssh-agent (to give anyone accees to the ssh-agent socket)
 	echo "Creating proxy socket..."
-	rm ${SSH_AUTH_SOCK} ${SSH_AUTH_PROXY_SOCK} > /dev/null 2>&1
+	rm ${SSH_AUTH_SOCK} ${SSH_AUTH_PROXY_SOCK} || true
 	socat UNIX-LISTEN:${SSH_AUTH_PROXY_SOCK},perm=0666,fork UNIX-CONNECT:${SSH_AUTH_SOCK} &
 	echo "Launching ssh-agent..."
 	# Start ssh-agent
