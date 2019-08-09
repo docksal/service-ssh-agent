25fda77f2198ab5dbdf63b5ba8b727cc115df563
Jelle Raaijmakers <jelle.raaijmakers@isaac.nl>
2019-08-09 17:16:40 +0200

    Use PID 1 process name instead of DOCKSAL_SSH_AGENT_USE_HOST

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-netstat -nlp | grep -qE "LISTENING.*${SSH_AUTH_PROXY_SOCK}"
-if [[ "$DOCKSAL_SSH_AGENT_USE_HOST" != "1" ]]; then
+# Get the name of the process with pid=1
+docker_cmd=$(cat /proc/1/comm)
+
+# Health checks for ssh-agent mode
+if [[ "${docker_cmd}" == "ssh-agent" ]]; then
+	netstat -nlp | grep -qE "LISTENING.*${SSH_AUTH_PROXY_SOCK}"
 	netstat -nlp | grep -qE "LISTENING.*${SSH_AUTH_SOCK}"
+fi
+
+# Health checks for ssh-proxy mode
+if [[ "${docker_cmd}" == "socat" ]]; then
+	netstat -nlp | grep -qE "LISTENING.*${SSH_AUTH_PROXY_SOCK}"
 fi
